230889c2eb4d1ac20edebb1c0999f650f2074866
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2024-08-25 17:08:51 +0530

    Delete Exercise 4 Online Bookstore - Processing Request Body and Form Data directory

--- Exercise 4 Online Bookstore - Processing Request Body and Form Data/BookstoreAPI/src/main/java/com/onlinebookstore/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlinebookstore.repository;
-
-import com.onlinebookstore.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
